7cdbb6c58f3bb610117373d3e99d87fd7110fd69
Adrien Szmul <aszmul@excilys.com>
2018-03-14 17:14:49 +0100

    End of serice for now, refactored

--- src/com/excilys/formation/java/computerdatabase/service/ValidatorComputer.java
+++ src/com/excilys/formation/java/computerdatabase/service/ValidatorComputer.java
@@ -3,7 +3,9 @@
  */
 package com.excilys.formation.java.computerdatabase.service;
 
+import com.excilys.formation.java.computerdatabase.model.Company;
 import com.excilys.formation.java.computerdatabase.model.Computer;
+import com.excilys.formation.java.computerdatabase.persistence.dao.CompanyDAO;
 
 /**
  * @author excilys
@@ -12,14 +14,23 @@ import com.excilys.formation.java.computerdatabase.model.Computer;
 public enum ValidatorComputer {
 
 	INSTANCE;
+	
+	private CompanyDAO companyDAO = CompanyDAO.INSTANCE;
 
-	public void validateComputer(Computer c) throws NullNameException {
+	public void validateComputer(Computer c) throws NullNameException, DateMismatchException, MissingCompanyException {
 		if(c.getName() == null) {
 			throw new NullNameException("Le nom de votre PC ne peut être nul !");
 		}
 		if(c.getDiscontinued() != null && c.getIntroduced() != null) {
 			if (c.getDiscontinued().isBefore(c.getIntroduced())) {
 				throw new DateMismatchException("La date discontinued ne peut être avant ou égal à la date introduced");
+			}
+		}
+		if(c.getCompany().getId() != null) {
+			Company ca = new Company();
+			ca.setId(c.getCompany().getId());
+			if (companyDAO.showDetails(ca) == null) {
+				throw new MissingCompanyException("L'id de company que vous avez donné n'existe pas !");
 			}
 		}
 	}
---
+++ src/com/excilys/formation/java/computerdatabase/ui/CommandLineInterface.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.excilys.formation.java.computerdatabase.ui;
+
+/**
+ * @author excilys
+ *
+ */
+public class CommandLineInterface {
+
+	/**
+	 * 
+	 */
+	public CommandLineInterface() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
